53912320ac3e3c6ba97636b8681dfa60c6776cfd
Stuart Runyan <shrunyan@gmail.com>
2015-12-17 16:57:51 -0800

    Code clean up

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 'use strict'
 
-let ec2RunInstance = require('./src/stages/ec2-run-instance')
-
 module.exports = {
   vendor: 'mc',
   name: 'aws',
   description: 'AWS package from space race.',
   stages: {
-    ec2_run_instance: ec2RunInstance
+    ec2_run_instance: require('./src/stages/ec2-run-instance')
   },
   logs: {}
 }
